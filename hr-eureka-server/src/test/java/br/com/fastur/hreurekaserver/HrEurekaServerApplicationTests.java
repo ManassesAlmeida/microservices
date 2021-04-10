38ad09d23e6e4a78bf4e0c6a81dfc592c2541c25
Manasses Almeida <junior.jta26@hotmail.com>
2021-04-09 21:20:17 -0300

    Project hr-eureka-server created.

---
+++ hr-eureka-server/src/test/java/br/com/fastur/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fastur.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
